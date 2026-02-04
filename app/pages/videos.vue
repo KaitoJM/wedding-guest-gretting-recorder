5064fbfd12b2387b9a1a8f228ec5ed939e616bad
JM Mac <johnmarkmancol@gmail.com>
2026-02-04 12:23:53 +0800

    chore: message on videos page if there are no videos yet

--- app/pages/videos.vue
+++ app/pages/videos.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4 p-4">
+  <div
+    v-if="videos.length === 0"
+    class="w-screen h-screen flex flex-col items-center justify-center"
+  >
+    <p>There are no videos saved yet.</p>
+  </div>
+  <div
+    v-else
+    class="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4 p-4"
+  >
     <VideoThumb v-for="video in videos" :key="video" :videoURL="video" />
   </div>
 </template>
