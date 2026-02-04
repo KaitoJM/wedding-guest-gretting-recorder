f0b650fe1014de387bba8c5d65b9aacccb9cf9a4
JM Mac <johnmarkmancol@gmail.com>
2026-02-04 11:26:51 +0800

    refactor: initialize recorder on component init

--- app/components/VideoMonitor.vue
+++ app/components/VideoMonitor.vue
@@ -10,7 +10,7 @@
 
     <div
       v-if="countDown > 0"
-      class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50"
+      class="absolute inset-0 flex items-center justify-center bg-black opacity-90 backdrop-blur-xl"
     >
       <div class="text-white text-[10rem] font-bold">{{ countDown }}</div>
     </div>
@@ -48,22 +48,17 @@ const formattedTime = computed(() => {
   return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 });
 
-let stream: MediaStream | null = null;
+const stream: MediaStream = await navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true,
+});
+
 let recorder: MediaRecorder | null = null;
 let chunks: Blob[] = [];
 let countdownInterval: NodeJS.Timeout | null = null;
 let recordingInterval: NodeJS.Timeout | null = null;
 
 const start = async () => {
-  stream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true,
-  });
-
-  if (video.value) {
-    video.value.srcObject = stream;
-  }
-
   recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (e) => chunks.push(e.data);
   recorder.start();
@@ -112,6 +107,10 @@ const cancelRecord = () => {
 };
 
 onMounted(() => {
+  if (video.value) {
+    video.value.srcObject = stream;
+  }
+
   countdownInterval = setInterval(() => {
     countDown.value--;
     if (countDown.value === 0) {
